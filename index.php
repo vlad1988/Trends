e1b489928a72782093ad9bb60578e7564d6ee940
Vlad <vladislafus@gmail.com>
2015-02-11 11:43:01 +0200

    Add some functional items

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php wp_head(); ?> 
+    </head>
+    <body <?php body_class(); ?>>
+        <p>Character set: <?php bloginfo('charset'); ?> </p>
+    </body>
+</html>
